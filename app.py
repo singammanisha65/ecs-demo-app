03b818fd67851500069d1fefff3a4c9aa90ba7aa
singammanisha65 <singammanisha65@gmail.com>
2025-07-30 22:17:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from your ECS app! 🚀</h1> after change in jenkins and it is getting deployed"
+    return "<h1>Hello from your ECS app! 🚀</h1> after change in jenkins and it is getting deploying in ALB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
